45d2f579e2fedc47c216664b6630ff0de6a975c3
savelov90 <Ev.savelov@gmail.com>
2020-12-03 15:54:53 +0300

    Module 18 Step 6 Buttons and Toasts

--- app/src/main/java/com/savelov/filmicpro/MainActivity.kt
+++ app/src/main/java/com/savelov/filmicpro/MainActivity.kt
@@ -2,11 +2,35 @@ package com.savelov.filmicpro
 
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMenuButtons()
+
     }
+
+    fun initMenuButtons() {
+        button_favorite.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        button_russian.setOnClickListener {
+            Toast.makeText(this, "Российское кино", Toast.LENGTH_SHORT).show()
+        }
+        button_top.setOnClickListener {
+            Toast.makeText(this, "Топ250", Toast.LENGTH_SHORT).show()
+        }
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        button_settings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
