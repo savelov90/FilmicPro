106ca64f292c23bd977975357d22f09dc41a789d
savelov90 <Ev.savelov@gmail.com>
2020-12-10 16:31:40 +0300

    Module 19 Finish my work on this module

--- app/src/main/java/com/savelov/filmicpro/MainActivity.kt
+++ app/src/main/java/com/savelov/filmicpro/MainActivity.kt
@@ -10,11 +10,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initMenuButtons()
-
+        initNavigation()
     }
 
-    fun initMenuButtons() {
+    fun initNavigation() {
+
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+/*    fun initMenuButtons() {
         button_favorite.setOnClickListener {
             Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
         }
@@ -30,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         button_settings.setOnClickListener {
             Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
         }
-    }
+    }*/
 
 
 }
