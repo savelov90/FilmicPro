fa04b9cea3ac73044b5f6d5aef8d43d92a49881d
savelov90 <Ev.savelov@gmail.com>
2021-03-25 17:36:52 +0300

    Module 33 API

---
+++ app/src/main/java/com/savelov/filmicpro/utils/TmdbApi.kt
@@ -0,0 +1,14 @@
+package com.savelov.filmicpro.utils
+
+import android.telecom.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TmdbApi {
+    @GET("3/movie/popular")
+    fun getFilms(
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String,
+        @Query("page") page: Int
+    ): Call<TmdbResults>
+}
